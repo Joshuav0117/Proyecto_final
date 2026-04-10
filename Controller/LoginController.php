2e5003c8ac19ebb753838e3f351b38adabc99897
Joshuav0117 <joshua.valentin2@upr.edu>
2026-04-10 14:53:53 -0400

    usuarios del login Hardcode

--- Controller/LoginController.php
+++ Controller/LoginController.php
@@ -7,6 +7,30 @@ class LoginController
         $error = '';
         $email = '';
 
+        // Diccionario de usuarios
+        $users = [
+            'aixa.ramirez@upr.edu' => [
+                'password' => 'pass1234',
+                'role' => 'admin'
+            ],
+            'joshua.valentin2@upr.edu' => [
+                'password' => 'pass1234',
+                'role' => 'admin'
+            ],
+            'michael.velez12@upr.edu' => [
+                'password' => 'pass1234',
+                'role' => 'director'
+            ],
+            'dereck.declet@upr.edu' => [
+                'password' => 'pass1234',
+                'role' => 'director'
+            ],
+            'usuario@upr.edu' => [
+                'password' => 'pass1234',
+                'role' => 'usuario'
+            ]
+        ];
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $email = trim($_POST['email'] ?? '');
             $password = trim($_POST['password'] ?? '');
@@ -14,15 +38,30 @@ class LoginController
             if ($email === '' || $password === '') {
                 $error = 'Debes completar todos los campos.';
             } else {
-                // Usuario de prueba
-                if ($email === 'admin@upr.edu' && $password === '123456') {
+                // Verificar usuario
+                if (isset($users[$email]) && $users[$email]['password'] === $password) {
+
                     $_SESSION['user'] = [
                         'email' => $email,
-                        'role' => 'admin'
+                        'role' => $users[$email]['role']
                     ];
 
-                    header('Location: index_admin.php');
+                    // Redirección según rol
+                    switch ($users[$email]['role']) {
+                        case 'admin':
+                            header('Location: index_admin.php');
+                            break;
+
+                        case 'director':
+                            header('Location: index_director.php');
+                            break;
+
+                        case 'usuario':
+                            header('Location: index_usuario.php');
+                            break;
+                    }
                     exit;
+
                 } else {
                     $error = 'Correo o contraseña incorrectos.';
                 }
